7215ef8c341c209909e8f280c009088e3c5734e9
Hanri <henry018@laposte.net>
2022-07-29 12:01:22 +0200

    ProductDetailScreen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -11,9 +11,37 @@ class ProductDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context)!.settings.arguments as String;
     final product = context.read<ProductsProvider>().productById(id);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(product.imageUrl, fit: BoxFit.cover,),
+          ),
+          const SizedBox(height: 10,),
+          Text(
+            '\$${product.price}',
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 20
+            ),
+          ),
+          const SizedBox(height: 10,),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            width: double.infinity,
+            child: Text(
+              product.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          )
+        ],
       ),
     );
   }
