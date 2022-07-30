aec2b7f9eacdc6a9333eb41b52f9f7188d0817d0
Hanri <henry018@laposte.net>
2022-07-30 17:24:33 +0200

    OrdersScreen best practice

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/models/order_model.dart';
 import 'package:shop_app/providers/orders_provider.dart';
 import 'package:shop_app/widgets/drawer_widget.dart';
 import 'package:shop_app/widgets/order_item_widget.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   static const String routeName = '/orders';
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+
+  late Future _futureOrders;
+
+  @override
+  void initState() {
+    _futureOrders = context.read<OrdersProvider>().fetchOrders();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +31,7 @@ class OrdersScreen extends StatelessWidget {
       ),
       drawer: const DrawerWidget(),
       body: FutureBuilder<void>(
-        future: context.read<OrdersProvider>().fetchOrders(),
+        future: _futureOrders,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator(),);
