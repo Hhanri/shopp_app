c67623a74f538bc7c47d6d6b5653c2a3c2b36905
Hanri <henry018@laposte.net>
2022-07-30 17:20:50 +0200

    OrdersScreen FutureBuilder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,54 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/models/order_model.dart';
 import 'package:shop_app/providers/orders_provider.dart';
 import 'package:shop_app/widgets/drawer_widget.dart';
 import 'package:shop_app/widgets/order_item_widget.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   static const String routeName = '/orders';
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-
-  bool loading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((value) async {
-      setState(() {
-        loading = true;
-      });
-      await context.read<OrdersProvider>().fetchOrders();
-      setState(() {
-        loading = false;
-      });
-    });
-
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersProvider = context.read<OrdersProvider>();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your orders"),
       ),
       drawer: const DrawerWidget(),
-      body: loading
-       ? const Center(child: CircularProgressIndicator(),)
-       : ListView.builder(
-           itemCount: ordersProvider.orders.length,
-           itemBuilder: (context, index) {
-             final currentOrder = ordersProvider.orders[index];
-             return OrderItemWidget(order: currentOrder);
-         },
-      ),
+      body: FutureBuilder<void>(
+        future: context.read<OrdersProvider>().fetchOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator(),);
+          }
+          return Consumer<OrdersProvider>(
+            builder:(context, ordersProvider, child) {
+              return  ListView.builder(
+                itemCount: ordersProvider.orders.length,
+                itemBuilder: (context, index) {
+                  final currentOrder = ordersProvider.orders[index];
+                  return OrderItemWidget(order: currentOrder);
+                },
+              );
+            }
+          );
+        }
+      )
     );
   }
 }
